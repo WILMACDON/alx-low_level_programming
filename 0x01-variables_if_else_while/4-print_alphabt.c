4dd12dd6100c90969d32ca012fecfdc1518fbfae
WILMACDON <wilmacson2101@gmail.com>
2022-03-11 13:19:18 -0800

    additions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main bock
+ * Description: prints alphabet leaving out q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
