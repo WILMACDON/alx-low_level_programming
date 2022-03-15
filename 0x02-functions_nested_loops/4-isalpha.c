8de56882a6cb6cb6364e77ad6a34961dce66f63b
WILMACDON <wilmacson2101@gmail.com>
2022-03-15 14:20:04 -0700

    additions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * _isalpha - check for lowercase
+ * @c : chararcter to check
+ * Return:0 or 1
+ */
+int_isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >=65 && c <= 90));
+
+}
