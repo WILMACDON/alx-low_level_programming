e8973979d541ad64a5f0fc80001779a2d633c1ba
WILMACDON <wilmacson2101@gmail.com>
2022-03-15 12:40:22 -0700

    readme file and first c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints the string "_putchar"
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+using functions and nested loops
