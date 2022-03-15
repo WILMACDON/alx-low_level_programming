922367622377f98571ebbf38564e68d26887f42a
WILMACDON <wilmacson2101@gmail.com>
2022-03-15 13:40:03 -0700

    additions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
  * print alphabet - prints the english alphabet from a-z.
  * Return:0
  */
+
 void print_alphabet(void)
 {
-	char c;
+	char c = 'a';
 
-	for (c = "a"; c<= 'z'; c++)
+	while (c <= 'z')
 	{
 		_putchar(c);
+		c++;
+
 	}
-	_putchar('\n')
+	_putchar('\n');
+
 }
