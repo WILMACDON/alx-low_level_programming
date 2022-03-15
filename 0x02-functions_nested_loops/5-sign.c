be7a91c0764374c02d21677277bec271e6e1b736
WILMACDON <wilmacson2101@gmail.com>
2022-03-15 15:10:39 -0700

    additions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - check numbers
  * @n : number to check
- * Return 0 or 1
+ * Return:0 or 1
  */
 
 int print_sign(int n)
@@ -20,7 +20,7 @@ int print_sign(int n)
 		test = 0;
 		_putchar('0');
 	}
-	else 
+	else
 	{
 		test = -1;
 		_putchar('-');
