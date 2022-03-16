baebb6dd6fc0eabf589201c726bc5293a6954684
WILMACDON <wilmacson2101@gmail.com>
2022-03-16 05:16:09 -0700

    additions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - returns the sum of it's paprameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
